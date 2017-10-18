5e5a2f0ee522128273fe3a4c45d022779e368e06
Prudhomme <aprudhom@gtlpc105.georgiatech-metz.fr>
2017-10-18 17:40:55 +0200

    Improved code Added charts

--- SP_server.py
+++ SP_server.py
@@ -1,6 +1,6 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SimpleHTTPServer
-from urlparse import parse_qs
+from urlparse import urlparse, parse_qs
 import cgi
 import os
 import json
@@ -9,21 +9,38 @@ import simplejson
 
 # import of Ros components to be able to subscribe to topics
 import rospy
-from std_msgs.msg import String
-from sensor_msgs.msg import CompressedImage
 
-lCamRate = None
-mCamRate = None
-rCamRate = None
-mCamImage = None
-subscribers = {'leftCameraRate': None, 'middleCameraImage': None, 'middleCameraRate': None, 'rightCameraRate': None}
+from std_msgs.msg import String
+
+from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Imu
+from sensor_msgs.msg import NavSatFix
+from sensor_msgs.msg import NavSatStatus
+from sensor_msgs.msg import TimeReference
+
+from vectornav.msg import ins
+from vectornav.msg import sensors
+
+subscribers = {}
 
 class CustomSubscriber:
-    def __init__(self, topicName, topicType, callback, subscriber = None):
+    def __init__(self, topicName, topicType, typesOfData, callback, subscriber = None):
         self.topicName = topicName
      	self.topicType = topicType
 	self.callback = callback
-        self.subscriber = rospy.Subscriber(topicName, topicType, callback)
+	self.typesOfData = typesOfData
+        self.subscriber = rospy.Subscriber(topicName, topicType, callback, (topicName,typesOfData))
+
+class SubscriberManager:
+    def __init__(self, customSubscriber, startTime = None, numberOfMessagesBetweenRequests = 0, lastData = None):
+        self.customSubscriber = customSubscriber
+     	self.startTime = startTime
+	self.numberOfMessagesBetweenRequests = numberOfMessagesBetweenRequests
+        self.lastData = lastData
+	
+class TypeOfData:
+    RATE, IMAGE, LIDAR, GPS, IMU, BATTERY, DISK_SPACE = range(7)
       
 class SPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def _set_headers(self):
@@ -35,39 +52,81 @@ class SPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def do_GET(self):
 	referer = self.headers.get('Referer')
-	print self.path
+	#print self.path
 
-	if self.path == '/getLeftCameraRate':
+	if '/getData' in self.path:
+	    self.send_response(200)
+	    self.data_string = parse_qs(urlparse(self.path).query)
+	    self.topicName = self.data_string['topicName'][0]
+	    self.typeOfData = int(self.data_string['typeOfData'][0])
+	    data = {}
+	    if TypeOfData.RATE == self.typeOfData:
+		self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+	        data['rate'] = computeAverage(self.topicName)
+	        json_data = json.dumps(data)
+	        self.wfile.write(json_data)
+	        return
+
+	    if TypeOfData.IMAGE == self.typeOfData:
+		self.send_header('Content-Type', 'image/jpeg')
+                self.end_headers()
+                if hasattr(subscribers[self.topicName], 'data') and subscribers[self.topicName].data is not None:
+	            data = subscribers[self.topicName].data
+	        self.wfile.write(data)
+		return
+
+	    if TypeOfData.LIDAR == self.typeOfData:
+		self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+		if hasattr(subscribers[self.topicName], 'data') and subscribers[self.topicName].data is not None:
+		    data['lidarAngleMin'] = subscribers[self.topicName].data.angle_min
+		    data['lidarAngleMax'] = subscribers[self.topicName].data.angle_max
+	    	    data['lidarData'] = subscribers[self.topicName].data.ranges
+	        json_data = json.dumps(data)
+	        self.wfile.write(json_data)
+	        return
+
+	    if TypeOfData.GPS == self.typeOfData:
+		self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+		data['latitude'] = subscribers[self.topicName].lastData.x
+	        data['longitude'] = subscribers[self.topicName].lastData.y
+	        data['altitude'] = subscribers[self.topicName].lastData.z
+	        json_data = json.dumps(data)
+	        self.wfile.write(json_data)
+		return
+
+	    if TypeOfData.IMU == self.typeOfData:
+		self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+		data['angular_velocity_x'] = subscribers[self.topicName].lastData.Gyro.x
+	        data['angular_velocity_y'] = subscribers[self.topicName].lastData.Gyro.y
+	        data['angular_velocity_z'] = subscribers[self.topicName].lastData.Gyro.z
+	        data['linear_acceleration_x'] = subscribers[self.topicName].lastData.Accel.x
+	        data['linear_acceleration_y'] = subscribers[self.topicName].lastData.Accel.y
+	        data['linear_acceleration_z'] = subscribers[self.topicName].lastData.Accel.z
+	        json_data = json.dumps(data)
+	        self.wfile.write(json_data)
+	        return
+
+	    if TypeOfData.BATTERY == self.typeOfData:
+		print 'not implemented yet'
+
+	    if TypeOfData.DISK_SPACE == self.typeOfData:
+		print 'not implemented yet'
+
+	if self.path == '/topics':
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
             data = {}
-	    if datas['leftCameraRate'] is not None:
-	    	data['rate'] = datas['leftCameraRate']
+	    for key in subscribers:
+		data[key] = subscribers[key].customSubscriber.typesOfData
 	    json_data = json.dumps(data)
 	    self.wfile.write(json_data)
 	    return
 
-	if self.path == '/getMiddleCameraRate':
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
-            data = {}
-	    if datas['middleCameraRate'] is not None:
-	    	data['rate'] = datas['middleCameraRate']
-	    json_data = json.dumps(data)
-	    self.wfile.write(json_data)
-	    return
-
-	if self.path == '/getMiddleCameraImage':
-            self.send_response(200)
-            self.send_header('Content-Type', 'image/jpeg')
-            self.end_headers()
-            data = {}
-	    if datas['middleCameraImage'] is not None:
-	    	data = datas['middleCameraImage']
-	    self.wfile.write(data)
-	    return
 	else:
 	    if self.path == '/':
 	        self.path = '/index.html'
@@ -77,56 +136,72 @@ class SPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_POST(self):
 
 	if self.path == '/unsub':
-	    print 'unsubscribing'
 	    self.data_string = self.rfile.read(int(self.headers['Content-Length']))
-	    print self.data_string
-	    subscribers[parse_qs(self.data_string)['key'][0]].subscriber.unregister()
+	    subscribers[parse_qs(self.data_string)['key'][0]].customSubscriber.subscriber.unregister()
+
 	if self.path == '/sub':
 	    self.data_string = self.rfile.read(int(self.headers['Content-Length']))
-	    customSub = subscribers[parse_qs(self.data_string)['key'][0]]
-	    subscribers[parse_qs(self.data_string)['key'][0]] = CustomSubscriber(customSub.topicName,customSub.topicType, customSub.callback)
-	
+	    customSub = subscribers[parse_qs(self.data_string)['key'][0]].customSubscriber
+	    subscribers[parse_qs(self.data_string)['key'][0]].customSubscriber = CustomSubscriber(customSub.topicName,customSub.topicType, customSub.typesOfData, customSub.callback)
 
-previousTimes = {'leftCameraRate': None, 'middleCameraRate': None, 'rightCameraRate': None}
-datas = {'leftCameraRate': None, 'middleCameraRate': None, 'rightCameraRate': None, 'middleCameraImage': None}
-def leftCameraRateCallback(data):
-    datas['leftCameraRate'] = getRate('leftCameraRate')
-    rospy.loginfo('Left camera rate: %s', datas['leftCameraRate'])
+#-------------Callback----------------
 
-def middleCameraRateCallback(data):
-    datas['middleCameraRate'] = getRate('middleCameraRate')
-    #rospy.loginfo('Middle camera rate: %s', datas['middleCameraRate'])
+def callback(data, args):
+    if TypeOfData.RATE in args[1]:
+	setTimeIfNecessary(args[0])
+	#rospy.loginfo(rospy.get_caller_id() + 'I received rate data')
 
-def middleCameraImageCallback(data):
-    #rospy.loginfo(rospy.get_caller_id() + 'I heard from mid %s', data)
-    #rospy.loginfo(rospy.get_caller_id() + 'I received datas')
-    datas['middleCameraImage'] = data
+    if TypeOfData.IMAGE in args[1]:
+	subscribers[args[0]].data = data
+	#rospy.loginfo(rospy.get_caller_id() + 'I received image data')
 
-def rightCameraRateCallback(data):
-    datas['rightCameraRate'] = getRate('rightCameraRate')
-    rospy.loginfo('Right camera rate: %s', datas['rightCameraRate'])
+    if TypeOfData.LIDAR in args[1]:
+	subscribers[args[0]].data = data
+	#rospy.loginfo(rospy.get_caller_id() + 'I received lidar data')
+
+    if TypeOfData.GPS in args[1]:
+	subscribers[args[0]].lastData = data.LLA
+	#rospy.loginfo(rospy.get_caller_id() + 'I received gps data')
+
+    if TypeOfData.IMU in args[1]:
+	subscribers[args[0]].lastData = data
+	#rospy.loginfo(rospy.get_caller_id() + 'I received imu data')
+
+    if TypeOfData.BATTERY in args[1]:
+	rospy.loginfo(rospy.get_caller_id() + 'I received battery data')
+
+    if TypeOfData.DISK_SPACE in args[1]:
+	rospy.loginfo(rospy.get_caller_id() + 'I received disk space data')
+    
+#---------------------------------------
 
 def subscribeToTopics():
     rospy.init_node('listener', anonymous=True)
-    #rospy.Subscriber('MiddleCameraRate', String, middleCameraRateCallback)
     
-    subscribers['middleCameraImage'] = CustomSubscriber('vrep/visionSensor/compressed',CompressedImage, middleCameraImageCallback)
-    subscribers['leftCameraRate'] = CustomSubscriber('leftCameraRate',String, leftCameraRateCallback)
-    subscribers['rightCameraRate'] = CustomSubscriber('rightCameraRate',String, rightCameraRateCallback)
-    subscribers['middleCameraRate'] = CustomSubscriber('vrep/visionSensor/compressed',CompressedImage, middleCameraRateCallback)
+    subscribers['vrep/visionSensor/compressed'] = SubscriberManager(CustomSubscriber('vrep/visionSensor/compressed', CompressedImage, [TypeOfData.RATE,TypeOfData.IMAGE], callback))
+    subscribers['lidar/scan'] = SubscriberManager(CustomSubscriber('lidar/scan', LaserScan, [TypeOfData.RATE,TypeOfData.LIDAR], callback))
+    subscribers['vectornav/imu'] = SubscriberManager(CustomSubscriber('vectornav/imu', sensors, [TypeOfData.IMU], callback))
+    subscribers['vectornav/ins'] = SubscriberManager(CustomSubscriber('vectornav/ins', ins, [TypeOfData.GPS], callback))
 
-def getRate(key):
-    rate = 0
-    if previousTimes[key] == None:
-        previousTimes[key] = rospy.Time.now()
-    else:
+# Methods to get the rate at which a topic is pusblishing
+# The parameter "key" is a string that contains the name of the topic (see previousTimes variable to get the key)
+def setTimeIfNecessary(key):
+    if subscribers[key].startTime == None:
+        subscribers[key].startTime = rospy.Time.now()
+    subscribers[key].numberOfMessagesBetweenRequests +=1
+
+def computeAverage(key):
+    averageRate = 0
+    if subscribers[key].startTime is not None:
         actualTime = rospy.Time.now()
-        deltaTime = actualTime - previousTimes[key]
-        rate = 1 * 1000000000 / float(str(deltaTime))
-        previousTimes[key] = rospy.Time.now()  
-    return rate
+        deltaTime = actualTime - subscribers[key].startTime
+        averageTimeBetweenMessages = deltaTime/subscribers[key].numberOfMessagesBetweenRequests
+        averageRate = 1 * 1000000000 / float(str(averageTimeBetweenMessages))
+        subscribers[key].startTime = None
+        subscribers[key].numberOfMessagesBetweenRequests = 0
+    return averageRate
 
-def run(server_class=HTTPServer, handler_class=SPRequestHandler, port=8089, server_ip='192.93.8.105'):
+def run(server_class=HTTPServer, handler_class=SPRequestHandler, port=8088, server_ip='192.93.8.105'):
     subscribeToTopics()
     web_dir = os.path.join(os.path.dirname(__file__), 'web')
     os.chdir(web_dir)
--- web/javascript.js
+++ web/javascript.js
@@ -1,5 +1,6 @@
 window.onload = function start() {
-    setInterval(updatedEvent, 2000);
+	askForTopics();
+    setInterval(updatedEvent, 1000);
 
     $('*[class^="col-"]').each(function() {
 
@@ -20,29 +21,142 @@ window.onload = function start() {
         });
 
     });
-    polarCanvas = document.getElementById("polarCanvas");
-    isPolarChartSet = false;
-    setInterval(updateChart, 2000);
-
+    lidarPolarCanvas = document.getElementById("lidarPolarCanvas");
+    isLidarPolarChartSet = false;
+	imageRateLineCanvas = document.getElementById("imageRateLineCanvas");
+    isImageRateChartSet = false;
+	updateLineChartRandomly();
 }
 var pressTimer;
-var isPolarChartSet;
-var polarAreaChart;
-var polarCanvas;
 
-function updateChart() {
+var isLidarPolarChartSet;
+var lidarPolarAreaChart;
+var lidarPolarCanvas;
+
+var isImageRateChartSet;
+var imageRateLineChart;
+var imageRateLineCanvas;
+
+TypeOfData = {
+	RATE: 0,
+ 	IMAGE: 1,
+ 	LIDAR: 2,
+ 	GPS: 3,
+ 	IMU: 4,
+ 	BATTERY: 5,
+ 	DISK_SPACE: 6
+};
+
+function updateChartRandomly() {
     var angleValues = new Array();
     var i;
     for (i = 0; i < 360; i++) {
         angleValues.push(Math.floor((Math.random() * 50) + 1));
     }
-    drawChart(angleValues);
+    drawLidarChart(angleValues);
+}
+
+function updateLineChartRandomly() {
+	if(!isImageRateChartSet){
+		var rateValues = new Array();
+		rateValues.push(0);
+		drawImageRateLineChart(rateValues);
+	}
+	//addNewRateToChart(Math.floor((Math.random() * 50) + 1));
+}
+
+function drawLidarChart(angleValues) {
+    if (lidarPolarCanvas != null) {
+        if (isLidarPolarChartSet == false) {
+			var radarChartLabels = new Array();
+			var i;
+			for (i = 0; i < angleValues.length; i++) {
+				radarChartLabels.push('');
+			}
+
+            lidarPolarAreaChart = new Chart(lidarPolarCanvas, {
+                type: 'radar',
+                data: {
+				labels: radarChartLabels,
+				datasets: [{
+					data: angleValues,
+					label: 'Lidar visualisation',
+					borderColor: "#f38b7a"
+				}]
+			}
+            });
+            isLidarPolarChartSet = true;
+			lidarPolarAreaChart.options.scale.angleLines.display = false;
+			lidarPolarAreaChart.options.animation.duration = 0;
+			lidarPolarAreaChart.options.elements.line.fill = false;
+			lidarPolarAreaChart.options.elements.line.borderWidth = 0.00001;
+			lidarPolarAreaChart.options.elements.point.radius = 1.5;
+			lidarPolarAreaChart.options.elements.point.pointStyle = "dot";
+			lidarPolarAreaChart.options.elements.point.backgroundColor = "#f38b7a";
+			lidarPolarAreaChart.update();
+        } else {
+            lidarPolarAreaChart.data.datasets[0].data = angleValues;
+            lidarPolarAreaChart.update();
+	    	//console.log(lidarPolarAreaChart);
+        }
+    }
+}
+
+function drawImageRateLineChart(rateValues) {
+    if (imageRateLineCanvas != null) {
+        if (isImageRateChartSet == false) {
+			var lineChartLabels = new Array();
+			var i;
+			for (i = 0; i < 100; i++) {
+				if (i % 10 == 0) {
+					lineChartLabels.push(i);
+				} else {
+					lineChartLabels.push("");
+				}
+			}
+            var lineData = {
+				labels: lineChartLabels,
+                datasets: [{
+                    data: rateValues,
+					label: 'Rate (Hz)',
+                    borderColor: "#f38b7a"
+                }]
+            };
+
+            imageRateLineChart = new Chart(imageRateLineCanvas, {
+                type: 'line',
+                data: lineData
+            });
+			imageRateLineChart.options.elements.point.radius = 0.5;
+			imageRateLineChart.options.scales.xAxes[0].showXLabels = 10;
+            isImageRateChartSet = true;
+        } else {
+            imageRateLineChart.data.datasets[0].data = rateValues;
+            imageRateLineChart.update();
+        }
+    }
+}
+
+function addNewRateToChart(newRate){
+	var values = imageRateLineChart.data.datasets[0].data;
+	if(values.length <100){
+		values.push(newRate);
+	}else{
+		values.splice(0,1);
+		values.push(newRate);
+	}
+	imageRateLineChart.data.datasets[0].data = values;
+    imageRateLineChart.update();
 }
 
 function changeActivity(element) {
 	if($(element).hasClass("inactive")){
+		console.log(element.id);
+		topics[element.id].subscribed = true;
 		subscribe(element.id);	
 	}else{
+		console.log(element.id);
+		topics[element.id].subscribed = false;
 		unsubscribe(element.id);
 	}
     $(element).toggleClass("inactive");
@@ -71,52 +185,69 @@ function redirectTo(element) {
     }
 }
 
-function drawChart(angleValues) {
-    if (polarCanvas != null) {
-        if (isPolarChartSet == false) {
-            var polarData = {
-                datasets: [{
-                    data: angleValues,
-                    backgroundColor: "#f38b4a",
-                    borderColor: "#f38b7a"
-                }]
-            };
-
-            polarAreaChart = new Chart(polarCanvas, {
-                type: 'polarArea',
-                data: polarData
-            });
-            console.log(polarAreaChart.options);
-            isPolarChartSet = true;
-        } else {
-            polarAreaChart.data.datasets[0].data = angleValues;
-            polarAreaChart.update();
-        }
-    }
-}
-
-
-
-function askForData(path, element, contentType, askingForRate) {
+function askForData(topicName, typeOfData) {
+	//console.log(topicName, typeOfData);
     $.ajax({
-        url: "http://192.93.8.105:8089/" + path,
+        url: "http://192.93.8.105:8088/" + "getData",
         type: "GET",
-        //contentType: contentType,
-        //dataType: contentType,
+		data: { topicName: topicName, typeOfData: typeOfData},
         success: function(data) {
-            console.log(data);
-			if(askingForRate){
+            //console.log(data);
+			var element = document.getElementById(topicName+typeOfData);
+			if(data.rate != undefined){
 				element.innerHTML = data.rate.toFixed(2) + " Hz";
-			}else{
+				if(topics[topicName].types.indexOf(TypeOfData.IMAGE) !== -1){
+					addNewRateToChart(data.rate.toFixed(2));		
+				}
+			}
+			if(data.lidarData != undefined){
+				drawLidarChart(data.lidarData);
+			}			
+			if(typeOfData == TypeOfData.IMAGE){
 				updateImage(data, element);
+			}
+			if(data.linear_acceleration_x != undefined){
+				var elements = element.getElementsByClassName("topicValue");
+				elements[0].innerHTML = data.linear_acceleration_x.toFixed(2);
+				elements[1].innerHTML = data.linear_acceleration_y.toFixed(2);
+				elements[2].innerHTML = data.linear_acceleration_z.toFixed(2);
+				elements[3].innerHTML = data.angular_velocity_x.toFixed(2);
+				elements[4].innerHTML = data.angular_velocity_y.toFixed(2);
+				elements[5].innerHTML = data.angular_velocity_z.toFixed(2);
+			}
+			if(data.latitude != undefined){
+				var elements = element.getElementsByClassName("topicValue");
+				elements[0].innerHTML = data.latitude.toFixed(7);
+				elements[1].innerHTML = data.longitude.toFixed(7);
+				elements[2].innerHTML = data.altitude.toFixed(2);
 			}
         },
         error: function(xhr, status, error) {
             var err = eval("(" + xhr.responseText + ")");
-            console.log("Error");
-            console.log(xhr);
-            console.log(status);
-            console.log(error);
+            //console.log("Error");
+            //console.log(xhr);
+            //console.log(status);
+            //console.log(error);
+        }
+    });
+}
+
+var topics = null;
+
+function askForTopics() {
+    $.ajax({
+        url: "http://192.93.8.105:8088/topics",
+        type: "GET",
+        success: function(data) {
+            
+			topics = data;
+			for (var topicName in topics) {
+				topics[topicName] = {types: topics[topicName], subscribed: true};
+			}
+			console.log(topics);
+        },
+        error: function(xhr, status, error) {
+            
         }
     });
 }
@@ -137,10 +268,8 @@ function updateImage(data, element){
 
 function setSubscribtion(path, topicName) {
     $.ajax({
-        url: "http://192.93.8.105:8089/" + path,
+        url: "http://192.93.8.105:8088/" + path,
         type: "POST",
-        contentType: "application/json",
-        dataType: "application/json",
         data: {
             key: topicName
         },
@@ -150,10 +279,10 @@ function setSubscribtion(path, topicName) {
         },
         error: function(xhr, status, error) {
             var err = eval("(" + xhr.responseText + ")");
-            console.log("Error");
-            console.log(xhr);
-            console.log(status);
-            console.log(error);
+            //console.log("Error");
+            //console.log('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);
+            //console.log(status);
+            //console.log(error);
         }
     });
 }
@@ -168,16 +297,32 @@ function Uint8ToString(u8a) {
 }
 
 function updatedEvent() {
-    var d = new Date();
-    //askForData("getLCamRate",document.getElementById("leftCameraRate"),"application/json");
-    //askForData("getMCamRate",document.getElementById("midCameraRate"),"application/json");
-    //askForData("getMCamImage",document.getElementById("image"),"image/jpeg");
-    if (document.getElementById("image") != null) {
-        askForData("getMiddleCameraImage", document.getElementById("image"), "image/jpeg",false);
-    }
-	if (document.getElementById("middleCameraRate") != null) {
-        askForData("getMiddleCameraRate", document.getElementById("middleCameraRateValue"), "application/json",true);
-    }
+	for (var topicName in topics) {
+		var typesOfData = topics[topicName].types;
+		if(topics[topicName].subscribed){
+			if (typesOfData.indexOf(TypeOfData.RATE) !== -1) {
+				askForData(topicName,TypeOfData.RATE);
+			}
+			if (typesOfData.indexOf(TypeOfData.IMAGE) !== -1) {
+			  	askForData(topicName,TypeOfData.IMAGE);
+			}
+			if (typesOfData.indexOf(TypeOfData.LIDAR) !== -1) {
+			 	askForData(topicName,TypeOfData.LIDAR);
+			}
+			if (typesOfData.indexOf(TypeOfData.GPS) !== -1) {
+			 	askForData(topicName,TypeOfData.GPS);
+			}
+			if (typesOfData.indexOf(TypeOfData.IMU) !== -1) {
+			 	askForData(topicName,TypeOfData.IMU);
+			}
+			if (typesOfData.indexOf(TypeOfData.BATTERY) !== -1) {
+			 	askForData(topicName,TypeOfData.BATTERY);
+			}
+			if (typesOfData.indexOf(TypeOfData.DISK_SPACE) !== -1) {
+				askForData(topicName,TypeOfData.DISK_SPACE);
+			}
+		}
+	}
 }
 
 function unsubscribe(topicName) {
